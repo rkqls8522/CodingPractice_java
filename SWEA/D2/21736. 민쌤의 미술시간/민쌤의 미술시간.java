fab2fa273213ce99a1a5c29a5453a2678ba5e373
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2024-08-09 16:12:03 +0900

    [D2] Title: 민쌤의 미술시간, Time: 115 ms, Memory: 19,144 KB -BaekjoonHub

--- SWEA/D2/21736. 민쌤의 미술시간/민쌤의 미술시간.java
+++ SWEA/D2/21736. 민쌤의 미술시간/민쌤의 미술시간.java
@@ -1,91 +1,87 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int t = 1; t <= T; t++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()); //행
-			int M = Integer.parseInt(st.nextToken()); //열
-			int K = Integer.parseInt(st.nextToken());
-			
-			int[][] arr = new int[N][M]; //0으로 초기화
-			//처음 칠하는 것이라면 그냥 칠할 것
-			boolean firstPaint = true;
-			
-			for (int i = 0; i < K; i++) { //K번 실행할 것
-				st = new StringTokenizer(br.readLine());
-				int startRow = Integer.parseInt(st.nextToken());
-				int startCol = Integer.parseInt(st.nextToken());
-				int endRow = Integer.parseInt(st.nextToken());
-				int endCol = Integer.parseInt(st.nextToken());
-				int color = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스
+        
+        // 색칠작업
+        // 직사각형으로만 색칠
+        // 물감색상 번호 0~10번
+        // 도화지는 처음에 전체가 0번 물감으로 채워짐
+        // 색칠하려는데 높은 색상이 색칠될 경우 -> 이번 색칠작업 x -> 다음 색칠로 넘어감
+        // 색칠 완료 후 가장 넓은 영역에 칠해진 색상번호의 영역크기 출력하기
+        
+        // 앞두개 행렬좌표
+        // 뒤두개 행렬좌표
+        // 색상번호
+    
+        
+        for (int testCase=1; testCase<=T; testCase++) { 
+            int[] answer = new int[11]; 
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken()); //  행의 갯수
+            int M = Integer.parseInt(st.nextToken()); // 열의 갯수
+            int K = Integer.parseInt(st.nextToken()); // 사각형 모양으로 색칠 작업할 횟수
+            answer[0] = N*M; 
+            
+            int[][] arr = new int[N][M]; // 0으로 채워진 초기판
+            
+            for (int i=0; i<K; i++) {
+                
+                st = new StringTokenizer(br.readLine());
 
-				if(firstPaint) {
-					pullPaint(arr, startRow, startCol, endRow, endCol, color);
-					firstPaint = false; //한번 칠했으니 이제 false로 바꿔주기
-				}
-				
-				//이미 더 높은 색이 칠해져 있는지 확인
-				boolean isNotPaint = isNotPainted(arr, startRow, startCol, endRow, endCol, color);
-				
-				//칠해도 된다면 칠해주기
-				if(isNotPaint) {
-					pullPaint(arr, startRow, startCol, endRow, endCol, color);
-				}
-			}
+                // (firstR, firstC)
+                int firstR = Integer.parseInt(st.nextToken());
+                int firstC = Integer.parseInt(st.nextToken());
+                // (secondR, secondC)
+                int secondR = Integer.parseInt(st.nextToken());
+                int secondC = Integer.parseInt(st.nextToken());
+                // 색상
+                int color = Integer.parseInt(st.nextToken());
+                
+                boolean isPainted = false;
+                
+                //기존보다 높은 색이 칠해져 있냐? 확인
+                out : for (int r = firstR; r <= secondR; r++) {
+                    for (int c = firstC; c <= secondC; c++) {
+                        if(arr[r][c] > color) {
+                            isPainted = true;
+                            break out;
+                        }
+                    }
+                }
+                
+                if(!isPainted) {
+                    // 기존보다 컬러가 값이 작은 부분만 새로운 색으로 색칠
+                    for (int r=firstR; r<=secondR; r++) {
+                        for (int c=firstC; c<=secondC; c++) {
+                            answer[arr[r][c]] -= 1;
+                            arr[r][c] = color;
+                            answer[color] += 1;
+//                            if(arr[r][c] < color) { // 새로 칠해지는 경우
 
-			//카운팅할 배열
-			int[] counting = new int[11]; //물감번호는 0부터 10까지 있음
-			
-			//다 칠했으면 이제 영역별로 카운팅하기
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < M; c++) {
-					counting[arr[r][c]]++; //해당 색상값 카운팅
-				}
-			}
-			
-			int max = Integer.MIN_VALUE;
-			//카운팅배열에서 max값 찾기
-			for (int i = 0; i < counting.length; i++) {
-				max = max > counting[i] ? max : counting[i];
-			}
-			
-			//출력할 값 만들기
-			sb.append("#").append(t).append(" ").append(max).append("\n");
-			
-			
-		}
-		System.out.print(sb);
-		
-	}
+                            } 
+                            
+                        }
+                    }
+                
+                }
+                Arrays.sort(answer);
+                sb.append("#").append(testCase).append(" ").append(answer[answer.length-1]).append("\n");
+            }
+            
+        System.out.print(sb);
 
-	public static void pullPaint(int[][] arr, int startRow, int startCol, int endRow, int endCol, int color) {
-		for (int r = startRow; r <= endRow; r++) {
-			for (int c = startCol; c <= endCol; c++) {
-				arr[r][c] =color; //칠해주기
-			}
-		}
-	}
-
-	public static boolean isNotPainted(int[][] arr, int startRow, int startCol, int endRow, int endCol, int color) {
-		for (int r = startRow; r <= endRow; r++) {
-			for (int c = startCol; c <= endCol; c++) {
-				if(arr[r][c] > color) {
-					return false; //칠해져있다면 false를 반환
-				}
-			}
-		}
-		return true; //안 칠해져 있다면 true반환
-	}
-
-}
+            
+        }
+        
+        
+    }
