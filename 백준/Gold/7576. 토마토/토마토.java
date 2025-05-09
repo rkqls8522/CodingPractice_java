eca2a812d9336e2c94deb74ce790293760e70b86
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2025-05-09 13:05:04 +0900

    [Gold V] Title: 토마토, Time: 564 ms, Memory: 112220 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -7,98 +7,106 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, map[][], badTomato;
-	static int[] dr = {-1,1,0,0}; //상하좌우
-	static int[] dc = {0,0, -1, 1}; //상하좌우
-	
-	//숙성된 토마토 위치를 보자. 숙성된 토마토를 저장하자
-	static Queue<Tomato> tomatoQueue = new LinkedList<>();
-	static class Tomato{
-		int r;
-		int c;
-		
-		public Tomato(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-		
-		@Override
-		public String toString() {
-			return "Tomato [r=" + r + ", c=" + c + "]";
-		}
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken()); //가로칸수
-		N = Integer.parseInt(st.nextToken()); //세로칸수
-		map = new int[N][M];
-		int timeResult = -1;
-		
-		badTomato = 0; //익혀야 하는 토마토의 갯수는 0개부터 cnt시작
-		
-		for (int r = 0; r < N; r++) {
-			st = new StringTokenizer(br.readLine());
-			for (int c = 0; c < M; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken());
-				if(map[r][c] == 1) {
-					tomatoQueue.add(new Tomato(r,c)); //익혀진 토마토면 토마토객체를 리스트에 추가
-				}
-				else if(map[r][c] == 0) badTomato++; //익혀야 하는 토마토 갯수카운트
-			}
-		}
+    //전역변수 관리
+    static int N, M;
+    static int[][] box;
+    static int tomatoCnt, ripeTomatoCnt, days;
+    static Queue<Integer> q;
 
-		if(badTomato == 0) { //익힐 토마토가 없으면
-			System.out.print(0); //0을 출력하고
-			return; //끝내
-		}
-		
-		//토마토 숙성 시뮬레이션
-		boolean ripen = true; //이번 시뮬레이션때 익혔니? 일단 true로 시작
-		while(ripen) { //익혔다면 새 시뮬레이션 시작. 더 익힐 것이 없다면 끝내
-			int tempBadTomatoCnt = badTomato; //기억해뒀다가 while문의 조건식에 쓰이기
-//			System.out.println("tempBadTomatoCnt : " + tempBadTomatoCnt);
-			
-			timeResult++;
-			ripen = false; //각 시뮬레이션마다 숙성여부 초기화
-			
-			//토마토를 리스트에서 하나씩 위치를 꺼내서 보자~
-			int tomatoListSize = tomatoQueue.size();
-			for (int i = 0; i < tomatoListSize; i++) {
-				Tomato tomato = tomatoQueue.poll();
-				int r = tomato.r;
-				int c = tomato.c;
-				
-				//상하좌우를 보고 만약 익힐 토마토가 옆에 있다면 익히자.
-				for (int d = 0; d < 4; d++) {
-					int row = r + dr[d];
-					int col = c + dc[d];
-					
-					//범위 벗어났는지 체크
-					if(row < N && col < M && row >= 0 && col >= 0) {
-						if(map[row][col] == 0) { //익혀야 하는 토마토니?
-//							System.out.print("row : " + row + ", col : " + col + ", badTomato : " + badTomato);
-							map[row][col] = 1; //익히자
-							badTomato--; //익혀야 할 토마토갯수가 줄었엉!!
-							tomatoQueue.add(new Tomato(row,col)); //익힌 토마토객체 추가
-						}					
-					}
-				}
-			}
-			
-//			System.out.println("badTomato : " + badTomato);
-			if(tempBadTomatoCnt != badTomato) { //숙성되지 않은 토마토의 갯수가 변경됐니?
-				ripen = true; //이번 시뮬레이션때 숙성을 했엉~
-			}
-		} //시뮬레이션 끝났어
-		
-		//토마토를 더 익힐 것이 없어서 시뮬레이션이 끝났는데,
-		//과연 다 익혔을까?
-		if(badTomato == 0) { //더이상 익힐 토마토가 없니?
-			System.out.print(timeResult); //그럼 시간 출력행
-		} else System.out.print(-1); //익힐 토마토가 남았다면 -1출력
-	}
+    public static void main(String[] args) throws IOException {
+
+        // 초기 설정
+        init();
+
+        //모든 토마토가 다 익기까지 걸리는 최소 날짜 계산
+        bfs();
+
+        //출력
+        int result = tomatoCnt > ripeTomatoCnt? -1 : days;
+        System.out.println(result);
+    }
+
+    private static void bfs() {
+
+        //변수 설정
+        int[] dr_arr = { -1, 0, 1, 0 }; //상, 우, 하, 좌
+        int[] dc_arr = { 0, 1, 0, -1 }; //상, 우, 하, 좌
+        int row, col, dr, dc, num, size;
+
+        //queue에서 하나씩 전부 꺼내며 주변의 0위치(익지 않은 토마토 위치)를 q에 추가.
+        //주변에 익지 않은 토마토가 하나도 없을 때까지 반복(큐에 값이 없어질 때까지)
+        while(!q.isEmpty()) {
+            size = q.size();
+
+            //하루 동안 익히기
+            for (int i = 0; i < size; i++) {
+
+                //변수 셋팅
+                num = q.poll();
+                row = num / M;
+                col = num % M;
+//                System.out.println("row = " + row + ", col = " + col);
+
+                //해당 위치에서 주변 상, 우, 하, 좌 위치 토마토 확인
+                for (int d = 0; d < 4; d++) { // 네 개의 방향
+                    dr = row + dr_arr[d];
+                    dc = col + dc_arr[d];
+
+                    // 범위를 벗어나지 않았고 익지 않은 토마토가 있는 위치라면
+                    if(check(dr, dc) && box[dr][dc] == 0){
+                        box[dr][dc] = 1; //토마토를 익히고
+                        ripeTomatoCnt++; //익은 토마토 갯수 추가
+                        q.add(dr*M + dc); //q에 추가
+                    }
+                }
+            }
+
+            // 하루 지났으니 days 증가
+            days++;
+        }
+
+        days--; //마지막 하루는 날짜 계산 x
+    }
+
+    //box범위 벗어나는지 체크
+    private static boolean check(int dr, int dc) {
+        if(dr < 0 || dc < 0 || dr >= N || dc >= M){
+            return false;
+        }
+        return true;
+    }
+
+    private static void init() throws IOException {
+
+        //버퍼를 이용하여 인풋값 빠르게 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //스트링 토크나이저 이용하여 인풋값 잘라 배열에 넣기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //초기 변수 설정
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        box = new int[N][M];
+        tomatoCnt = 0; //토마토 총 갯수 세기
+        ripeTomatoCnt = 0; //익은 토마토 총 갯수 세기
+        days = 0; //총 걸리는 날짜 수 계산
+
+        // 큐 사용. (r,c)위치를 넣을 것이므로 배열을 담을 queue임.
+        q = new LinkedList<>();
+
+        // 초기 맵(박스)셋팅
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                box[i][j] = Integer.parseInt(st.nextToken());
+                if(box[i][j] != -1) tomatoCnt++; //빈 칸이 아니라면 토마토갯수 증가.
+                if(box[i][j] == 1) {
+                    ripeTomatoCnt++; //익은 토마토라면 익은 토마토갯수 증가.
+                    q.add(M*i+j); // queue에도 추가. 배열 말고 숫자로만 넣자
+                }
+            }
+        }
+    }
 }
