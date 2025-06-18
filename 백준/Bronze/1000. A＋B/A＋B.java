c54e042e7ee96c99a2dcb7cd5b2ca9ab23622593
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2025-06-18 22:58:29 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(a + b);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
