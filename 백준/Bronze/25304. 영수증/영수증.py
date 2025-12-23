219e56141fc967ea20d0ac8e8b40c17cd96d8337
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2025-12-24 03:15:26 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,14 @@
+# 25304 영수증
+
+total = int(input())
+n = int(input())
+sum = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    sum += a * b
+
+if total == sum:
+    print('Yes')
+else:
+    print('No')
