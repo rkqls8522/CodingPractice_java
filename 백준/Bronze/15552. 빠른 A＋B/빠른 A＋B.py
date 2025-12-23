f923439c620ffdcd6abab314fa5b3e6ddae38fd5
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2025-12-24 03:37:37 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 996 ms, Memory: 97572 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -3,11 +3,11 @@
 import sys
 
 input = sys.stdin.readline
+write = sys.stdout.write  # 보통 많은 출력값을 join과 함께 사용할 때 좋다.
+out = []
 
-write = sys.stdout.write
-
-t = int(input())
-
-for _ in range(t):
+for _ in range(int(input())):
     a, b = map(int, input().split())
-    write(str(a + b) + "\n")
+    out.append(str(a + b))
+
+write("\n".join(out))
