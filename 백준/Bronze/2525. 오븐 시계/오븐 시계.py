1af52c0aa0d4f362b9eb048397be22601469229e
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2025-12-24 02:35:34 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,16 @@
+# 2525 오븐 시계
+
+hour, minute = map(int, input().split())
+time = int(input())
+
+hour += time // 60
+minute += time % 60
+
+if minute >= 60 :
+    hour += 1
+    minute -= 60
+
+if hour >= 24 :
+    hour -= 24
+
+print(hour, minute)
