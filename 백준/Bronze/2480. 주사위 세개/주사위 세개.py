7f7a50cc2624cda3833b4b8598197b16fde16de4
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2025-12-24 02:33:29 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,4 +1,7 @@
-n1,n2,n3 = map(int,input().split(' '))
+# 2480 주사위 세개
+
+n1,n2,n3 = map(int,input().split())
+
 if n1 == n2 and n2 == n3:
     print(10000+n1*1000)
 elif n1 != n2 and n2 != n3 and n1 != n3:
