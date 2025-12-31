e6ee769b9d97c6c5e1d91d60957ad5c9562b4ad6
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2025-12-31 10:45:47 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,12 +1,12 @@
 N, M = map(int, input().split())
 
-list = [i+1 for i in range(N)]
+list = [i for i in range(N+1)]
 
 for _ in range(M) :
     i, j = map(int, input().split())
     
-    temp = list[i-1]
-    list[i-1] = list[j-1]
-    list[j-1] = temp
+    temp = list[i]
+    list[i] = list[j]
+    list[j] = temp
 
-print(" ".join(str(num) for num in list))
+print(" ".join(str(num) for num in list[1:]))
